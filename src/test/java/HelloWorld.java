395e15e1ba1b8b3d45271de9ae8c49cbc9e6ecc8
Darek1966 <dar.ch983@gmail.com>
2022-11-12 11:45:06 +0100

    Unit

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!!");
+        hello();
+        helloDarek();
+    }
+
+    public static void hello() {
+        System.out.println("Hello!!!");
+    }
+
+    public static void helloDarek() {
+        System.out.println("Hello Darek!!!");
+    }
+}
---
+++ src/test/java/HelloWorldJUnit.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class HelloWorldJUnit {
+
+    @Test
+    public void hello() {
+        System.out.println("Hello!!!");
+    }
+
+    @Test
+    public void hello1() {
+        System.out.println("Hello1!!!");
+    }
+}
